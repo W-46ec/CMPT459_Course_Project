31345537dc49b0750d5dc29306109422db8e37ef
W-46ec <30034417+W-46ec@users.noreply.github.com>
2020-11-19 09:39:40 -0800

    Milestone 2.2 - GaussianNB

--- Milestone_2/src/2.2_GaussianNB.py
+++ Milestone_2/src/2.2_GaussianNB.py
@@ -2,33 +2,25 @@
 import numpy as np
 import pandas as pd
 import pickle
-from sklearn.preprocessing import LabelEncoder
 from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 
 
 def main():
-    input_data_file = "../dataset/2.0_cases_cleaned.csv.gz"
-    data = pd.read_csv(input_data_file)
+    X_train_inputfile = "../dataset/2.1_X_train.csv.gz"
+    X_valid_inputfile = "../dataset/2.1_X_valid.csv.gz"
+    y_train_inputfile = "../dataset/2.1_y_train.csv.gz"
+    y_valid_inputfile = "../dataset/2.1_y_valid.csv.gz"
 
-    to_encode = ['sex', 'province', 'country', 'outcome']
-    le = LabelEncoder()
-    for i in range(len(to_encode)):
-        data[to_encode[i]] = le.fit_transform(data[to_encode[i]].astype(str))
-
-    print(data)
-
-    X, y = data, data['outcome'].to_numpy()
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size = 0.2)
+    X_train = pd.read_csv(X_train_inputfile)
+    X_valid = pd.read_csv(X_valid_inputfile)
+    y_train = pd.read_csv(y_train_inputfile).transpose().values[0]
+    y_valid = pd.read_csv(y_valid_inputfile).transpose().values[0]
 
     # classify with Gaussian Naive Bayes model
     nb_model = make_pipeline(
         GaussianNB()
     )
-
-    nb_model.fit(X_train, y_train)
-    print("Validation score (NB):", nb_model.score(X_valid, y_valid))  
     
     nb_pkl = '../models/nb_classifier.pkl'
     pickle.dump(nb_model, open(nb_pkl, 'wb'))
